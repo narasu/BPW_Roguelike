6545f4f7540a3b626174059716d6528325466998
Michael Yamada <roninnarasu@gmail.com>
2021-02-17 20:45:41 +0100

    deleted old astar

--- Assets/Scripts/Enemies/Crawler.cs
+++ Assets/Scripts/Enemies/Crawler.cs
@@ -1,16 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding;
 public class Crawler : Enemy
 {
-    AIDestinationSetter destinationSetter;
     
-    private void Start()
-    {
-        
-        destinationSetter = GetComponent<AIDestinationSetter>();
-        destinationSetter.target = player;
-    }
 }
 
